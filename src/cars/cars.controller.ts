acfd136f49344b4235ba4d2837a43f2d08123ad3
Frank712 <franksuriel_7@hotmail.com>
2023-11-08 19:46:53 -0600

    Add getCarById method in car controller

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Mustang'];
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Mustang'];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id) {
+    return this.cars[id];
   }
 }

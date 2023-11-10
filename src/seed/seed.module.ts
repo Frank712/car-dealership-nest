0b1eca98c2c2d7dc84ffc7b277a931cfb915fa66
Frank712 <franksuriel_7@hotmail.com>
2023-11-10 01:11:49 -0600

    update readme

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { CarsService } from 'src/cars/cars.service';
-import { BrandsService } from 'src/brands/brands.service';
 import { CarsModule } from 'src/cars/cars.module';
 import { BrandsModule } from 'src/brands/brands.module';
 
